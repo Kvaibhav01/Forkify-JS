adf45f1a65ce58df3de0141425b4142b9cefbc52
Jesu Castillo <castillojesuj@gmail.com>
2019-09-25 23:32:17 -0300

    fix(searchview/highlightselected): check if querySelector returns null before using classList prop
    Since document.querySelector can return null if no element that matches is in the DOM, we have to
    check for it not being null
    
    fix #2

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,22 +1,25 @@
-import { elements } from './base';
+import { elements } from "./base";
 
 export const getInput = () => elements.searchInput.value;
 
 export const clearInput = () => {
-    elements.searchInput.value = '';
+  elements.searchInput.value = "";
 };
 
 export const clearResults = () => {
-    elements.searchResList.innerHTML = '';
-    elements.searchResPages.innerHTML = '';
+  elements.searchResList.innerHTML = "";
+  elements.searchResPages.innerHTML = "";
 };
 
 export const highlightSelected = id => {
-    const resultsArr = Array.from(document.querySelectorAll('.results__link'));
-    resultsArr.forEach(el => {
-        el.classList.remove('results__link--active');
-    });
-    document.querySelector(`.results__link[href*="${id}"]`).classList.add('results__link--active');
+  const resultsArr = Array.from(document.querySelectorAll(".results__link"));
+  resultsArr.forEach(el => {
+    el.classList.remove("results__link--active");
+  });
+  const resultLinks = document.querySelector(`.results__link[href*="${id}"]`);
+  if (resultLinks) {
+    resultLinks.classList.add("results__link--active");
+  }
 };
 
 /*
@@ -28,76 +31,82 @@ acc: 15 / acc + cur.length = 18 / newTitle = ['Pasta', 'with', 'tomato']
 acc: 18 / acc + cur.length = 24 / newTitle = ['Pasta', 'with', 'tomato']
 */
 export const limitRecipeTitle = (title, limit = 17) => {
-    const newTitle = [];
-    if (title.length > limit) {
-        title.split(' ').reduce((acc, cur) => {
-            if (acc + cur.length <= limit) {
-                newTitle.push(cur);
-            }
-            return acc + cur.length;
-        }, 0);
+  const newTitle = [];
+  if (title.length > limit) {
+    title.split(" ").reduce((acc, cur) => {
+      if (acc + cur.length <= limit) {
+        newTitle.push(cur);
+      }
+      return acc + cur.length;
+    }, 0);
 
-        // return the result
-        return `${newTitle.join(' ')} ...`;
-    }
-    return title;
-}
+    // return the result
+    return `${newTitle.join(" ")} ...`;
+  }
+  return title;
+};
 
 const renderRecipe = recipe => {
-    const markup = `
+  const markup = `
         <li>
             <a class="results__link" href="#${recipe.recipe_id}">
                 <figure class="results__fig">
                     <img src="${recipe.image_url}" alt="${recipe.title}">
                 </figure>
                 <div class="results__data">
-                    <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
+                    <h4 class="results__name">${limitRecipeTitle(
+                      recipe.title
+                    )}</h4>
                     <p class="results__author">${recipe.publisher}</p>
                 </div>
             </a>
         </li>
     `;
-    elements.searchResList.insertAdjacentHTML('beforeend', markup);
+  elements.searchResList.insertAdjacentHTML("beforeend", markup);
 };
 
 // type: 'prev' or 'next'
 const createButton = (page, type) => `
-    <button class="btn-inline results__btn--${type}" data-goto=${type === 'prev' ? page - 1 : page + 1}>
-        <span>Page ${type === 'prev' ? page - 1 : page + 1}</span>
+    <button class="btn-inline results__btn--${type}" data-goto=${
+  type === "prev" ? page - 1 : page + 1
+}>
+        <span>Page ${type === "prev" ? page - 1 : page + 1}</span>
         <svg class="search__icon">
-            <use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
+            <use href="img/icons.svg#icon-triangle-${
+              type === "prev" ? "left" : "right"
+            }"></use>
         </svg>
     </button>
 `;
 
 const renderButtons = (page, numResults, resPerPage) => {
-    const pages = Math.ceil(numResults / resPerPage);
+  const pages = Math.ceil(numResults / resPerPage);
 
-    let button;
-    if (page === 1 && pages > 1) {
-        // Only button to go to next page
-        button = createButton(page, 'next');
-    } else if (page < pages) {
-        // Both buttons
-        button = `
-            ${createButton(page, 'prev')}
-            ${createButton(page, 'next')}
+  let button;
+  if (page === 1 && pages > 1) {
+    // Only button to go to next page
+    button = createButton(page, "next");
+  } else if (page < pages) {
+    // Both buttons
+    button = `
+            ${createButton(page, "prev")}
+            ${createButton(page, "next")}
         `;
-    } else if (page === pages && pages > 1) {
-        // Only button to go to prev page
-        button = createButton(page, 'prev');
-    }
+  } else if (page === pages && pages > 1) {
+    // Only button to go to prev page
+    button = createButton(page, "prev");
+  }
 
-    elements.searchResPages.insertAdjacentHTML('afterbegin', button);
+  elements.searchResPages.insertAdjacentHTML("afterbegin", button);
 };
 
 export const renderResults = (recipes, page = 1, resPerPage = 10) => {
-    // render results of currente page
-    const start = (page - 1) * resPerPage;
-    const end = page * resPerPage;
+  // render results of currente page
+  const start = (page - 1) * resPerPage;
+  const end = page * resPerPage;
 
-    recipes.slice(start, end).forEach(renderRecipe);
+  recipes.slice(start, end).forEach(renderRecipe);
 
-    // render pagination buttons
-    renderButtons(page, recipes.length, resPerPage);
+  // render pagination buttons
+  renderButtons(page, recipes.length, resPerPage);
 };
